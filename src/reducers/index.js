772746ca11b4c6a7101bab4af11ed4f4e294875d
unknown <alan.freedrive2@gmail.com>
2020-11-09 10:06:03 -0600

    stock-app: removed handle-error-reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,7 @@
 import {combineReducers} from 'redux';
 
-import handleError from 'reducers/handle-error/handle-error';
 import handleStockOverview from 'reducers/handle-stock-overview/handle-stock-overview';
 const allReducers = combineReducers({
-  error: handleError,
   stockOverview: handleStockOverview,
 })
 
